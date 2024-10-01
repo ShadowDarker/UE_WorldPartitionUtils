efcbbf21f535ce739dfebe6a1874cde01e48fb5f
Shadow <ShadowDarker@users.noreply.github.com>
2024-10-01 11:38:50 +0200

    Initial commit

---
+++ WorldPartitionUtils/Libraries/WorldPartitionBlueprintFunctionLibrary.cpp
@@ -0,0 +1,43 @@
+﻿#include "Libraries/WorldPartitionBlueprintFunctionLibrary.h"
+#include "WorldPartitionStreaming/DataLayerStreamingData.h"
+#include "WorldPartition/DataLayer/DataLayerAsset.h"
+#include "WorldPartition/DataLayer/DataLayerManager.h"
+
+bool UWorldPartitionBlueprintFunctionLibrary::HandleDataLayerStreamingOperations(UObject* WorldContextObject, TArray<FDataLayerStreamingData> DataLayerStreamingDataContainers)
+{
+	if (DataLayerStreamingDataContainers.IsEmpty())
+	{
+		return false;
+	}
+
+	UDataLayerManager* DataLayerManager = UDataLayerManager::GetDataLayerManager(WorldContextObject);
+
+	if (DataLayerManager == nullptr)
+	{
+		return false;
+	}
+	
+	for (FDataLayerStreamingData DataLayerStreamingData : DataLayerStreamingDataContainers)
+	{
+		for (UDataLayerAsset* DataLayerAsset : DataLayerStreamingData.DataLayers)
+		{
+			if (DataLayerAsset == nullptr)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Tried to set state on null Datalayer reference"));
+				continue;
+			}
+			
+			const bool bSuccess = DataLayerManager->SetDataLayerInstanceRuntimeState(DataLayerManager->GetDataLayerInstance(DataLayerAsset), DataLayerStreamingData.DataLayerRuntimeState, DataLayerStreamingData.bRecursive);
+
+			if (!bSuccess)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, FString::Printf(TEXT("Failed to set runtime state %s in DataLayer: %s"), GetDataLayerRuntimeStateName(DataLayerStreamingData.DataLayerRuntimeState), *DataLayerAsset->GetName()));
+				UE_LOG(LogTemp, Warning, TEXT("Failed to set runtime state %s in DataLayer: %s"), GetDataLayerRuntimeStateName(DataLayerStreamingData.DataLayerRuntimeState), *DataLayerAsset->GetName());
+				
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
---
+++ WorldPartitionUtils/Libraries/WorldPartitionBlueprintFunctionLibrary.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WorldPartitionBlueprintFunctionLibrary.generated.h"
+
+struct FDataLayerStreamingData;
+/**
+ * 
+ */
+UCLASS()
+class WORLDPARTITIONUTILS_API UWorldPartitionBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "DataLayer")
+	static bool HandleDataLayerStreamingOperations(UObject* WorldContextObject, TArray<FDataLayerStreamingData> DataLayerStreamingDataContainers);
+};
